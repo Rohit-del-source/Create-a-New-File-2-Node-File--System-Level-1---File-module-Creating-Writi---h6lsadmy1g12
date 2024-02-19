ff89e7fa35c555a2cbf06826333708e9115d9709
Rohit-del-source <rohitverma18158920@gmail.com>
2024-02-19 22:37:40 +0530

    initial

--- src/index.js
+++ src/index.js
@@ -4,6 +4,15 @@ const fileContent = "Newton School";
 const writeFile = async (fileName, fileContent) => {
     //Write your code here
     //Don't change function name
+    try {
+        await fs.access(fileName)
+        console.log("myfile.txt already exists. Skipping write operation");
+    } catch (error) {
+        
+        await fs.writeFile(fileName, fileContent);
+        console.log("File myfile.txt created and data written successfully!")
+    }
   
 };
+
 module.exports =  writeFile ;
